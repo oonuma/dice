db2abf453780467a135c8246459edea3c0eb3c80
masahito <oonumaster@gmail.com>
2009-12-09 15:55:22 +0900

    add dice projectfiles

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,8 +1,27 @@
 class DiceController < ApplicationController
   def index 
+		@rolls = Roll.find(:all, :order => "created_at DESC", :limit => 5 )
   end
+
   def shake	
 		dice = Dice.new
 		@result = dice.shake
+		roll = Roll.new
+		roll.value = @result
+		roll.save
   end
+
+	def gamble
+		if request.post?
+			#丁半の判定
+			#@result = :win
+			dealer = Dealer.new
+      @result = dealer.chouhan(params[:expected])
+		end	
+	end
+
+	def report
+		@count = Roll.count
+		@statistics = Roll.count(:group => :value)
+	end	
 end
